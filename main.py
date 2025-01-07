47576f5af2ba8ed06d04d824086a8bd862729ad2
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-01-06 19:18:58 -0500

    Step 1: Learning about match code block.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('This is the start of my Python journey!')
+todos = []
+
+while True:
+    user_action = input('Please type add or show: ')
+
+    match user_action:
+        case 'add':
+            todo = input('Please type a todo: ')
+            todos.append(todo)
+        case 'show':
+            print(todos)
+
+# user_action should be part of the while loop.
+# If user_action is moved out of the while loop block,
+# we create an infinite loop.
