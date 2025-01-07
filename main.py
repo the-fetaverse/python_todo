1cb281df9917874c547c01e0993b368374814866
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-01-06 19:42:41 -0500

    Step 5: upper() and capitalize().

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ todos = []
 
 while True:
     user_action = input('Please type ADD, SHOW or EXIT: ')
-    user_action = user_action.strip().capitalize()
+    user_action = user_action.strip().upper()
 
     match user_action:
         case 'ADD':
             todo = input('Please type a todo: ')
+            todo = todo.capitalize()
             todos.append(todo)
         case 'SHOW':
             print('Here is a list of your todos:')
