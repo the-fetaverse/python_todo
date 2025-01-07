88619a09c6cf4a1db0ccecb988ce2ecf72f970f0
Giannis Sporidis <ioannis.sporidis@gmail.com>
2025-01-06 21:08:29 -0500

    Step 1: Learn about indexing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input('Please type ADD, SHOW or EXIT: ')
+    user_action = input('Please type ADD, SHOW, EDIT or EXIT: ')
     user_action = user_action.strip().upper()
 
     match user_action:
@@ -11,8 +11,14 @@ while True:
             todos.append(todo)
         case 'SHOW':
             print('Here is a list of your todos:')
-            for item in todos:
-                print(item)
+            for i in range(len(todos)):
+                print(i + 1, todos[i])
+        case 'EDIT':
+            i = int(input('Type the number of the todo to edit: '))
+            temp_todo = todos[i - 1]
+            new_item = input('Type the new todo: ')
+            todos[i - 1] = new_item
+            print('Previous name:', temp_todo, 'New name:', new_item)
         case 'EXIT':
             break
 
